14bc4eaa289fbcfc8bf3e79a709e53a61f68d26e
Furuta Naoki <naoki.furuta@japan-systems.co.jp>
2025-09-30 14:04:08 +0900

    fix: Add logging for collected user IDs in fetchAndSaveStudyData function

--- scripts/fetch_supabase.ts
+++ scripts/fetch_supabase.ts
@@ -39,7 +39,7 @@ const fetchAndSaveStudyData = async () => {
   console.log('studies:', studies);
   // userIdを集める
   const userIds = Array.from(new Set(studies.map(s => s.userId).filter((id): id is number => id !== undefined)));
-
+  console.log('userIds:', userIds);
   // usersデータ取得
   const { data: users, error: userError } = await supabase
     .from('users')
