4e8b9fb0f3c803ce294fb73f61513be39cafe2de
Furuta Naoki <naoki.furuta@japan-systems.co.jp>
2025-10-03 14:20:22 +0900

    fix(document): change HTML language attribute to Japanese and add default metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,23 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ja">
       <Head>
         <link rel="icon" href="/favicon.png" />
+
+        {/* デフォルトメタデータ */}
+        <meta name="description" content="YouTubeチャンネルの記録を可視化するアナリティクスアプリ" />
+
+        {/* デフォルトOGP設定 */}
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="アナリティクスアプリ" />
+        <meta property="og:locale" content="ja_JP" />
+        <meta property="og:description" content="YouTubeチャンネルの記録を可視化するアナリティクスアプリ" />
+        <meta property="og:image" content="https://aew2sbee.github.io/favicon.png" />
+
+        {/* デフォルトTwitter Card設定 */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="https://aew2sbee.github.io/favicon.png" />
       </Head>
       <body className="antialiased">
         <Main />
