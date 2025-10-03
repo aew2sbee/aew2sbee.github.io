f939d04865517a61e434a2fa999177401bcbe4eb
Furuta Naoki <naoki.furuta@japan-systems.co.jp>
2025-10-03 16:49:47 +0900

    fix(document): add default metadata for improved SEO and social sharing

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,18 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-V41BQHBSBJ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-V41BQHBSBJ');
+            `,
+          }}
+        />
         <link rel="icon" href="/favicon.png" />
 
         {/* デフォルトメタデータ */}
